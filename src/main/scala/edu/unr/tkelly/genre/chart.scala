11bcb94211a178a9c01fe79c402535dfb6a17b1b
Thomas Kelly <tjbk123@gmail.com>
2012-10-23 00:45:13 -0700

    Reformatted tabs

--- src/main/scala/edu/unr/tkelly/genre/chart.scala
+++ src/main/scala/edu/unr/tkelly/genre/chart.scala
@@ -7,44 +7,46 @@ import breeze.linalg.DenseMatrix
 import scala.Array.canBuildFrom
 
 object Chart extends App {
-	def plotSongFromStyle(plot: Plot, style: String) = {
-	  val query = new PlaylistParams
-	  
-	  // Compose a query
-	  query.add("style", style)
-	  query.add("bucket", "audio_summary")
-	  query.add("type", "artist-description")	  
-	  
-	  val song = Util.getSongs(query, 1).head
-	  val features = song.getAnalysis().getSegments().toArray(manifest[Segment])
-	  val featureCount = features(0).getTimbre().length + 
-	                     features(0).getPitches().length
-	  val timbres =
-	    for (segment <- features;
-	          tFeature <- segment.getTimbre) yield tFeature
-	  val timbreMin = timbres.min
-	  val timbreMax = timbres.max
-	  
-	  val featureMat = DenseMatrix.tabulate(24, features.length) (
-	      (r:Int, c: Int) => {
-	  	    if (r < 12) {
-	  	      (features(c).getTimbre()(r) - timbreMin) / (timbreMax - timbreMin)
-	  	    } else {
-	  	      features(c).getPitches()(r-12)
-	  	    }
-	  	  })
-	  plot.title = style ++ " - " ++ song.getArtistName() ++ " - " ++
-	            song.getTitle()
-	  plot += image(featureMat, new GradientPaintScale(0, 1, PaintScale.Rainbow))
-	}
-	
-	val en = new EchoNestAPI("KRPNFJRX9QKTVBG70")
-	val fig = Figure()
-	
-	plotSongFromStyle(fig.subplot(3, 2, 0), "classical")
-	plotSongFromStyle(fig.subplot(3, 2, 2), "classical")
-	plotSongFromStyle(fig.subplot(3, 2, 4), "classical")
-	plotSongFromStyle(fig.subplot(3, 2, 1), "rock")
-	plotSongFromStyle(fig.subplot(3, 2, 3), "metal")
-	plotSongFromStyle(fig.subplot(3, 2, 5), "pop")
+  def plotSongFromStyle(plot: Plot, style: String) = {
+    val query = new PlaylistParams
+
+    // Compose a query
+    query.add("style", style)
+    query.add("bucket", "audio_summary")
+    query.add("type", "artist-description")
+
+    val song = Util.getSongs(query, 1).head
+    val features = song.getAnalysis().getSegments().toArray(manifest[Segment])
+    val featureCount = features(0).getTimbre().length +
+      features(0).getPitches().length
+    val timbres =
+      for (
+        segment <- features;
+        tFeature <- segment.getTimbre
+      ) yield tFeature
+    val timbreMin = timbres.min
+    val timbreMax = timbres.max
+
+    val featureMat = DenseMatrix.tabulate(24, features.length)(
+      (r: Int, c: Int) => {
+        if (r < 12) {
+          (features(c).getTimbre()(r) - timbreMin) / (timbreMax - timbreMin)
+        } else {
+          features(c).getPitches()(r - 12)
+        }
+      })
+    plot.title = style ++ " - " ++ song.getArtistName() ++ " - " ++
+      song.getTitle()
+    plot += image(featureMat, new GradientPaintScale(0, 1, PaintScale.Rainbow))
+  }
+
+  val en = new EchoNestAPI("KRPNFJRX9QKTVBG70")
+  val fig = Figure()
+
+  plotSongFromStyle(fig.subplot(3, 2, 0), "classical")
+  plotSongFromStyle(fig.subplot(3, 2, 2), "classical")
+  plotSongFromStyle(fig.subplot(3, 2, 4), "classical")
+  plotSongFromStyle(fig.subplot(3, 2, 1), "rock")
+  plotSongFromStyle(fig.subplot(3, 2, 3), "metal")
+  plotSongFromStyle(fig.subplot(3, 2, 5), "pop")
 }
--- src/main/scala/edu/unr/tkelly/genre/util.scala
+++ src/main/scala/edu/unr/tkelly/genre/util.scala
@@ -11,31 +11,31 @@ import scala.concurrent.forkjoin.ForkJoinPool
 
 object Util {
   val api = new EchoNestAPI("KRPNFJRX9QKTVBG70")
-  
+
   // Gets any number of unique songs following specified parameters
   def getSongs(params: PlaylistParams, qty: Int) = {
     val sessionID = api.createDynamicPlaylist(params).getSession
 
     // Gets one song, however many tries it takes
     def getSong(): Song =
-        try {
-          // Try to get a song
-          api.getNextInDynamicPlaylist(sessionID).getSongs().get(0)
-        } catch {
-          // Catching Throwable is bad practice, but getNextInDynamicPlaylist 
-          // is badly behaved and may throw any number of Exceptions and Errors
-          case (_: Throwable) => {
-            // If anything bad happens, wait one second
-            Thread.sleep(1000)
-            // And try again
-            getSong()
-          }
+      try {
+        // Try to get a song
+        api.getNextInDynamicPlaylist(sessionID).getSongs().get(0)
+      } catch {
+        // Catching Throwable is bad practice, but getNextInDynamicPlaylist 
+        // is badly behaved and may throw any number of Exceptions and Errors
+        case (_: Throwable) => {
+          // If anything bad happens, wait one second
+          Thread.sleep(1000)
+          // And try again
+          getSong()
         }
+      }
 
     // Gets all of the unique songs needed
     /* Note that the reason that we need all of this fanciness is that,
      * when we grab songs in parallel, we may get duplicate songs.
-     */ 
+     */
     def getUniqueSongs(accum: Set[Song]): Set[Song] = {
       if (accum.size >= qty) {
         // If we have enough, return the songs
@@ -43,7 +43,7 @@ object Util {
       } else {
         // In parallel, try to get however many songs we are short
         val songs = for (_ <- (1 to (qty - accum.size)).par) yield getSong()
-        
+
         // Hold on to the union of the songs already obtained and the songs
         // we just got
         getUniqueSongs(accum ++ songs)
